47908100406c1a56e962ae004efaa4048d228ac9
ytisf <yuvaln210@gmail.com>
2014-10-30 09:29:39 +0200

    first codes

---
+++ includes/email_modules.py
@@ -0,0 +1,131 @@
+# coding=utf-8
+import re
+import csv
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+
+import includes.when_things_go_south
+
+
+class MailModule():
+	"""
+	Tיhis is the main module that is responsible for all the email handeling
+	It parses the CSV and goes even to the sending of the email and the attaching
+	of the images to the HTML body.
+	"""
+	def __init__(self, server, port):
+		class Object(object):
+			"""
+			General object class to be used later
+			"""
+			def __init__(self):
+				pass
+
+		self._error_handler = includes.when_things_go_south.Error_Handler()       # Error event handler
+
+		# Build mail server options
+		self._mailserver = Object()
+		self._mailserver_server = server
+		self._mailserver_port = port
+		self._folder = "sample_conf/"
+		self._i = 0                     # counter for retrying
+		self._j = 2                     # how many times to try
+
+	def parse_csv(self, filename):
+		"""
+		:param filename: Location of CSV file with emails
+		:return: Array of addresses
+		"""
+		try:
+			listi = []
+			f = open(filename, 'rb')
+			list = csv.reader(f, delimiter=";")
+			self._error_handler.log_error(0, "Parsed CSV file successfully")
+			for data in list:
+				listi.append(data)
+			return listi
+		except:
+			self._error_handler.log_error(3, "Could not read address file. Please verify location entered.")
+
+
+	def send_email(self, from_email, rcpt_email, subject, html_body, text_body, attachments, counter, total_len):
+		"""
+		:param from_email: The email to send from
+		:param rcpt_email: Email address to send to
+		:param subject: Subject of the mail
+		:param html_body: HTML Body as string
+		:param text_body: Text body as string
+		:return:
+		"""
+
+		# Checking for retry counter
+		if self._i == self._j:
+			self._i = 1
+			return 0
+
+		self._error_handler.log_error(0, "Building mail")
+		msg = MIMEMultipart('related')
+		msg['Subject'] = subject
+		msg['From'] = from_email
+		msg['To'] = rcpt_email
+		msg.preamble = 'This is a multi-part message in MIME format.'
+
+		msgAlternative = MIMEMultipart('alternative')
+		msg.attach(msgAlternative)
+
+		msgText = MIMEText(text_body)
+		msgAlternative.attach(msgText)
+
+		# Starting to handle images in file:
+		html_body = html_body.replace('<img src="', '<img src="cid:')
+		html_body = html_body.replace('<image src="', '<img src="cid:')
+
+		msgText = MIMEText(html_body, 'html')
+		msgAlternative.attach(msgText)
+
+		matchObj = re.search(r"<img src=\"cid:(.+)\"", html_body)
+
+		if matchObj:
+			for image in matchObj.groups():
+				fp = open(self._folder + image, 'rb')
+				msgImage = MIMEImage(fp.read())
+				fp.close()
+				name_of_image = image.split('/')
+				name_of_image = name_of_image[:1]
+				name_of_image = str(name_of_image)
+				name_of_image = name_of_image[2:]
+				name_of_image = name_of_image[:-2]
+				msgImage.add_header('Content-ID', name_of_image,)
+				msg.attach(msgImage)
+		else:
+			pass
+
+		# Done with the Images
+
+		# Start with attachments:
+		if attachments:
+			for fi in attachments:
+				f = file(fi)
+				attachment = MIMEText(f.read())
+				attachment.add_header('Content-Disposition', 'attachment', filename=fi)
+				msg.attach(attachment)
+		# End with Attachments
+
+
+		# Sending email!
+		try:
+			s = smtplib.SMTP(str(self._mailserver_server))
+			self._error_handler.log_error(0, "Connected to SMTP server successfully")
+		except smtplib.SMTPConnectError:
+			self._error_handler.log_error(3, "Could not connect to mail server: %s" % self._mailserver_server)
+		try:
+			s.sendmail(from_email, rcpt_email, msg.as_string())
+			self._error_handler.log_error(0, "[" + str(counter) + "/" + str(total_len) + "] Mail sent from " + from_email + " to " + rcpt_email)
+		except:
+			self._error_handler.log_error(2, "[" + str(counter) + "/" + str(total_len) + "] Mail sending failed from " + from_email + " to " + rcpt_email)
+			# self._error_handler.log_error(2, "Trying one more time...")
+			# self._i += 1
+			# self.send_email(from_email, rcpt_email, subject, html_body, text_body, attachments)
+		s.quit()
---
+++ includes/when_things_go_south.py
@@ -0,0 +1,42 @@
+import sys
+
+
+class Error_Handler():
+	"""
+	This is just a simple and simple error handler
+	"""
+	def __init__(self):
+		pass
+
+	class bcolors:
+		HEADER = '\033[95m'
+		OKBLUE = '\033[94m'
+		OKGREEN = '\033[92m'
+		WARNING = '\033[93m'
+		FAIL = '\033[91m'
+		ENDC = '\033[0m'
+
+	def log_error(self, id, error):
+		"""
+		:param id: Identifier of error type
+		:param error: Error message
+		:return: nothing
+		"""
+
+		# 0 = info, 1=warning, 2=error, 3=critical
+
+		if id == 0:
+			sys.stdout.write(self.bcolors.OKGREEN + "[+] " + self.bcolors.ENDC + error + "\n")
+
+		elif id == 1:
+			sys.stderr.write(self.bcolors.WARNING + "[-] " + self.bcolors.ENDC + error + "\n")
+
+		elif id == 2:
+			sys.stderr.write(self.bcolors.FAIL + "[!] " + self.bcolors.ENDC + error + "\n")
+
+		elif id == 3:
+			sys.exit(self.bcolors.FAIL + "[X] " + self.bcolors.ENDC + error + "\n")
+
+		else:
+			print("Go learn your error handler!!!!")
+			sys.exit(1)
